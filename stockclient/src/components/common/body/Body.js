81134ee9419915b3023b39025423bd0347ded804
unknown <bubbastinx@gmail.com>
2019-09-25 12:52:17 -0500

    updated Body component

--- stockclient/src/components/common/body/Body.js
+++ stockclient/src/components/common/body/Body.js
@@ -3,9 +3,9 @@ import React from 'react'
  function Body() {
 	return (
 		<main>
-			<h3>This is the main Component</h3>
+			<h3>MERN STACK DOCKER TEMPLATE</h3>
 
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio alias iure libero quidem, cumque magni sapiente earum accusantium molestias dignissimos suscipit mollitia tempora, laboriosam maxime natus laudantium eligendi dolorem.</p>
+			<p>This is a MERN template that has docker containers</p>
 		</main>
 	)
 }
